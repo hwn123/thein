04f961bd653faead17eb05fc63bd452ad7649685
hwn123 <holywan@yandex.ru>
2019-02-22 19:01:28 +0300

    Initial commit.

---
+++ src/main/java/com/kalvinov/theing/app/WebContoroller.java
@@ -0,0 +1,24 @@
+package com.kalvinov.theing.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class WebContoroller {
+    @Value("${node_name}")
+    private String node_name;
+
+    @GetMapping("/status")
+    @ResponseBody
+    public Map<String, String> status(){
+        Map<String, String> statusMap = new HashMap();
+        statusMap.put("status", "healthy");
+        statusMap.put("node_name", node_name);
+        return statusMap;
+    }
+}
